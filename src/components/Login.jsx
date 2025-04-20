eed32b607c0b8fa620f1dc955f60d6f81e2931ca
vickyemms <64374691+vickyemms@users.noreply.github.com>
2025-04-20 20:34:35 +0200

    fix login styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/Login.css";
 
 const Login = ({ onLoginSuccess, onSwitchToSignup }) => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -10,7 +10,7 @@ const Login = ({ onLoginSuccess, onSwitchToSignup }) => {
     event.preventDefault();
 
     const loginRequest = {
-      email: username,
+      email: email,
       password: password,
     };
 
@@ -37,45 +37,47 @@ const Login = ({ onLoginSuccess, onSwitchToSignup }) => {
   };
 
   return (
-    <div className="login-container">
+    <div>
       <header>
         <h1 className="logo">Polyglot</h1>
       </header>
-      <div className="login-form">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="login-input-group">
-            <label htmlFor="username">Email:</label>
-            <input
-              className="login-input"
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              className="login-input"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="login-btns">
-            <button className="login-btn" type="submit">
-              Log In
-            </button>
-            <button className="signup-btn" onClick={onSwitchToSignup}>
-              Sign up
-            </button>
+      <div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <h2 className="login-header">Login</h2>
+          <div className="login-form-items">
+            <div className="login-input-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                className="login-input"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="login-input-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                className="login-input"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="login-btns">
+              <button className="login-btn" type="submit">
+                Log In
+              </button>
+              <button className="signup-btn" onClick={onSwitchToSignup}>
+                Sign up
+              </button>
+            </div>
+            {errorMessage && <div className="error">{errorMessage}</div>}
           </div>
         </form>
-        {errorMessage && <div className="error">{errorMessage}</div>}
       </div>
     </div>
   );
